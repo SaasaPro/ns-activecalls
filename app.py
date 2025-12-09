270768f29e819b79d928d50c1bd767facec84598
Luis Cerna <162777576+SaasaPro@users.noreply.github.com>
2025-12-09 16:56:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # habilita CORS globalmente
 
-API_URL = "https://portal4.myvrv.com/ns-api/domains/verve.myvrv.com/calls"
+API_URL = "https://portal4.myvrv.com/ns-api/v2/domains/verve.myvrv.com/calls"
 ACCESS_TOKEN = "nss_LJ4V1HYTyfFk2LMWZlTwMKCKKz1WplCT9sgNySFdeno6r8Kx349dee24"
 
 @app.route("/active-calls")
